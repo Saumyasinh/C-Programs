dbdc0d3be1d2d4ab2c7dc947d2a5a94db6145640
Saumyasinh <saumyasinhmori@gmail.com>
2024-11-08 15:04:15 +0530

    Add files via upload

---
+++ p 14 average .c.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+main()
+{
+	int maggie,petrol,recharge;
+	int average;
+	
+	printf("\nEnter maggie value =>");
+	scanf("%d",&maggie);
+	
+	printf("\nEnter petrol value =>");
+	scanf("%d",&petrol);
+	
+	printf("\nEnter recharge value =>");
+	scanf("%d",&recharge);
+	
+	average =maggie+petrol+recharge;
+	
+	printf("\naverage=%d",average);
+}
+	
+	
+
